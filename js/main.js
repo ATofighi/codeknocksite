e48df2438d04fb7a722057653ed5f7573dba5d53
Amir Nadiri <amir77ni@gmail.com>
2018-03-03 23:18:33 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-FlipClock.Lang.persian = {
-    days: "روز",
-    hours: "ساعت",
-    minutes: "دقیقه",
-    months: "ماه",
-    seconds: "ثانیه",
-    years: "سال"
-};
-var clock = $('.clock').FlipClock((Date.parse("9 March 2018 18:30:00 GMT+0330") - Date.now()) / 1000, {
-    clockFace: 'DailyCounter',
-    countdown: true,
-    language: 'persian',
-});
+var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
+var days, hours, minutes, seconds; // variables for time units
+
+var countdown = document.getElementById("tiles"); // get tag element
+
+getCountdown();
+
+setInterval(function () { getCountdown(); }, 1000);
+
+function getCountdown(){
+
+	// find the amount of "seconds" between now and target
+	var current_date = new Date().getTime();
+	var seconds_left = (target_date - current_date) / 1000;
+
+	days = pad( parseInt(seconds_left / 86400) );
+	seconds_left = seconds_left % 86400;
+		 
+	hours = pad( parseInt(seconds_left / 3600) );
+	seconds_left = seconds_left % 3600;
+		  
+	minutes = pad( parseInt(seconds_left / 60) );
+	seconds = pad( parseInt( seconds_left % 60 ) );
+
+	// format countdown string + set tag value
+	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+}
+
+function pad(n) {
+	return (n < 10 ? '0' : '') + n;
+}
+
 
 var pages = ['body', "#time", "#about", '#past_contests', '#prizes', '#sponser', '#contact'];
 
